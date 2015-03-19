5f213601ca8d9ce9a64e4878b4e160caf6736d38
roman <trexbazarov@gmail.com>
2015-03-19 14:41:54 +0200

    bug fixes

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language'  => 'en-US',
     'modules'=>[
         'user' => [
             'class' => 'amnah\yii2\user\Module',
