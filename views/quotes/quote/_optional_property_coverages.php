e63337b2dd5f73378be0640d13c5ffcf840ce525
roman <trexbazarov@gmail.com>
2015-04-01 16:27:01 +0300

    bug fixes

--- views/quotes/quote/_optional_property_coverages.php
+++ views/quotes/quote/_optional_property_coverages.php
@@ -418,7 +418,7 @@ if ($model->propertyCoverages->refrigerated_food) {
     <tr>
         <td><?= $model->propertyCoverages->getFormNumber('refrigerated_food') ?></td>
         <td colspan="2"><?= $model->propertyCoverages->getAttributeLabel('refrigerated_food') ?></td>
-        <td>><?= Yii::$app->formatter->asCurrency($model->propertyCoverages->refrigerated_food) ?></td>
+        <td><?= Yii::$app->formatter->asCurrency($model->propertyCoverages->refrigerated_food) ?></td>
         <td><?= Yii::$app->formatter->asCurrency($refrigeratedFoodPremium) ?></td>
         <td><?php echo Yii::$app->quote->getValueByAttribute($model->propertyCoverages,'food_deductible')?></td>
     </tr>
