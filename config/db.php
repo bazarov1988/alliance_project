59f2a20f5020098c54d19f2b664bd531b5c40f32
roman <trexbazarov@gmail.com>
2015-04-30 15:19:56 +0300

    settings form

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=alliance',
     'username' => 'admin', //'admin',
-    'password' => '3DPa11sw85', //'3DPa11sw85#6kf',
+    'password' => '3DPa11sw85#6kf', //'3DPa11sw85#6kf',
     'charset' => 'utf8',
 ];
