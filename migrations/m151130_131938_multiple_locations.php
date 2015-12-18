ac4db9e87c34be93413410fbea6352e05a1e67c8
roman <trexbazarov@gmail.com>
2015-12-18 18:43:35 +0200

    all formulas

--- migrations/m151130_131938_multiple_locations.php
+++ migrations/m151130_131938_multiple_locations.php
@@ -7,19 +7,19 @@ class m151130_131938_multiple_locations extends Migration
 {
 	public function up()
 	{
-//		$this->execute("
-//			CREATE TABLE IF NOT EXISTS `quotes_locations` (
-//			  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-//			  `quote_id` int(11) NOT NULL,
-//			  `occupancy_id` int(11) NOT NULL,
-//			  PRIMARY KEY (`id`)
-//			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-//		");
+		$this->execute("
+			CREATE TABLE IF NOT EXISTS `quotes_locations` (
+			  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+			  `quote_id` int(11) NOT NULL,
+			  `occupancy_id` int(11) NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+		");
 
-//		$this->execute("
-//	    ALTER TABLE `quotes_locations` ADD `clergypersons` INT( 11 ) NOT NULL DEFAULT '0' AFTER `occupancy_id` ,
-//		ADD `clergypersons_liability` INT( 1 ) NOT NULL DEFAULT '0' AFTER `clergypersons` ;
-//	    ");
+		$this->execute("
+	    ALTER TABLE `quotes_locations` ADD `clergypersons` INT( 11 ) NOT NULL DEFAULT '0' AFTER `occupancy_id` ,
+		ADD `clergypersons_liability` INT( 1 ) NOT NULL DEFAULT '0' AFTER `clergypersons` ;
+	    ");
 		$this->execute(
 			"ALTER TABLE `bop_rater_entry` ADD `asbestos_exclusion` INT( 11 ) NOT NULL DEFAULT '0';"
 		);
